bc17bc436261c4d1c1ff8e78d51f1943bbda9cb1
Fabrizio Galiano <fabrizio.galiano@hotmail.com>
2017-07-09 10:53:34 +0200

    fix entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/env bash
-
 # if [ "$1" = "start-stack" ]; then
-
     #bootloading for configuration
     /bin/bash /docker/scripts/bootloader.sh
-
     #call supervisord to launch the whole stack
     /usr/bin/supervisord --nodaemon --configuration=/docker/configuration/supervisord/supervisord.conf
-
 # fi
